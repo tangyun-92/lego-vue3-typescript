f1c528bf761f48266b7f5476aeeb7fa01af7d484
tangyun <tangyun1992@qq.com>
2022-02-18 14:25:34 +0800

    引入ant-design-vue并注册

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,8 @@
+/*
+* @Author: TangYun
+* @Date: 2022/2/18 10:53
+* @Description: 允许.vue文件导入--定义文件
+*/
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
